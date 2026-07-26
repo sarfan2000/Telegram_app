e0e7e2ff4404b01e00a1639dc4add699e56258fb
sarfan2000 <sarfan5577@gmail.com>
2026-07-26 23:11:43 +0530

    fix: remove hardcoded google dns in production to fix railway deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,9 +8,11 @@ const mongoose = require('mongoose')
  */
 const connectDB = async () => {
   try {
-    // FIX: Fallback to Google DNS to prevent "querySrv ECONNREFUSED" on some networks (like mobile hotspots)
-    const dns = require('dns')
-    dns.setServers(['8.8.8.8', '8.8.4.4'])
+    // FIX: Fallback to Google DNS to prevent "querySrv ECONNREFUSED" on some networks (like mobile hotspots) only in dev
+    if (process.env.NODE_ENV === 'development') {
+      const dns = require('dns')
+      dns.setServers(['8.8.8.8', '8.8.4.4'])
+    }
 
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       family: 4 // Force IPv4
